a772948c9d1cd9543e8a949ea2c1ee2b69b3551e
deniskadrich <dev4long@live.com>
2021-03-23 22:21:20 -0400

    comnpleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById('main');
+main.remove()
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Denis is the champion'
